40f52b599bbe6e89add43c3ff8535bac66dc50d7
shravan kotha <shravan.kotha@icmrl.net>
2023-07-19 15:02:29 -0400

    Calculation of homogenized specific heat from the imposed volumetric heat flux is added. Tested

--- extractSpecificHeatFromAbaqusOdb.py
+++ extractSpecificHeatFromAbaqusOdb.py
@@ -8,21 +8,23 @@ from odbSection import *
 from textRepr import *
 import time as wallTime
 
-if len(sys.argv) != 6:
-   text = """Five command line arguments are expected: \
+if len(sys.argv) != 7:
+   text = """Six command line arguments are expected: \
             \n\t(1) odbName with extension \
-            \n\t(2) output directory path \
-            \n\t(3) direction of the thermal gradient applied (X/Y/Z) \
-            \n\t(4) different in surface heat flux applied along the above direction (J/(m^2-sec)) \
-            \n\t(5) frame increment Number"""
+            \n\t(2) Output directory path \
+            \n\t(3) Volumetric heat flux (J/(m^3-sec)) \
+            \n\t(4) Initial temperature \
+            \n\t(5) Homogenized density \
+            \n\t(6) Frame increment Number"""
    raise RuntimeError(text)
     
 # Settings
 odbName = str(sys.argv[1])
 outputPath = sys.argv[2]
-directionGradientApplied = str(sys.argv[3])
-appliedResultantHeatFlux = float(sys.argv[4])
-frameNoInc = int(sys.argv[5])             # useful if no of frames are too large - first and last frames are always exported
+volumetricHeatFlux = float(sys.argv[3])
+volumetricTemp_initial = float(sys.argv[4])
+density = float(sys.argv[5])
+frameNoInc = int(sys.argv[6])             # useful if no of frames are too large - first and last frames are always exported
 
 # extract these from the odb
 odb=openOdb(path=odbName)
@@ -32,80 +34,38 @@ stepObject=odb.steps[stepName[0]]
 assembly = odb.rootAssembly
 elType=assembly.instances[instanceName[0]].elements[0].type
 print('Element Type: ',str(elType))    
+totalNoFrames=len(stepObject.frames) 
+totalTimeSteps=totalNoFrames-1     
    
 # open files for writing stresses and nodal disp for extensometer region
-outputFileSpecificHeatEffective=open('effectiveSpecificHeat.out','w')
-
-totalNoFrames=len(stepObject.frames) 
-totalTimeSteps=totalNoFrames-1  
+outputFileSpecificHeatEffective=open('homogenizedSpecificHeat.out','w')
+outputFileSpecificHeatEffective.write("{0:35s}{1:35s}{2:35s}{3:35s}{4:35s}\n".format('time','homogenizedDensity','InitialTemp','volumetricTemperature','homogenizedSpecificHeat'))
 
 start = wallTime.clock()
-
-time_old = 0
-volumetricTemp_old = 0
-volumetricHeatFlux_1_old = 0
 for frameNumber in range(1,totalNoFrames):
-    # get frame data and print time to both the files
     frameData = stepObject.frames[frameNumber]
     time=frameData.frameValue
-    print('Frame Number : ',frameNumber,'   Time : ',time)
    
     elementIPVolumeObject = frameData.fieldOutputs['IVOL']
     elementIPVolumeFieldValues = elementIPVolumeObject.values
-    elementHeatFluxObject = frameData.fieldOutputs['HFL']
-    elementHeatFluxFieldValues = elementHeatFluxObject.values
-    elementGradTObject = frameData.fieldOutputs['GRADT']
-    elementGradTFieldValues = elementGradTObject.values
     elementTempObject = frameData.fieldOutputs['TEMP']
     elementTempFieldValues = elementTempObject.values
     
     totalVolume = 0
-    volumetricHeatFlux_1 = 0
-    volumetricHeatFlux_2 = 0
-    volumetricHeatFlux_3 = 0
-    volumetricGradT_1 = 0
-    volumetricGradT_2 = 0
-    volumetricGradT_3 = 0
     volumetricTemp = 0
-    for iVol, iHeatFlux, iGradT, iTemp in zip(elementIPVolumeFieldValues,elementHeatFluxFieldValues,elementGradTFieldValues,elementTempFieldValues):
+    for iVol, iTemp in zip(elementIPVolumeFieldValues,elementTempFieldValues):
         volumeIP = iVol.data
         totalVolume = totalVolume + volumeIP
-        volumetricHeatFlux_1 = volumetricHeatFlux_1 + iHeatFlux.data[0]*volumeIP
-        volumetricHeatFlux_2 = volumetricHeatFlux_2 + iHeatFlux.data[1]*volumeIP
-        volumetricHeatFlux_3 = volumetricHeatFlux_3 + iHeatFlux.data[2]*volumeIP
-        volumetricGradT_1 = volumetricGradT_1 + iGradT.data[0]*volumeIP
-        volumetricGradT_2 = volumetricGradT_2 + iGradT.data[1]*volumeIP
-        volumetricGradT_3 = volumetricGradT_3 + iGradT.data[2]*volumeIP
         volumetricTemp = volumetricTemp + iTemp.data*volumeIP
         
-    volumetricHeatFlux_1 = volumetricHeatFlux_1/totalVolume
-    volumetricHeatFlux_2 = volumetricHeatFlux_2/totalVolume
-    volumetricHeatFlux_3 = volumetricHeatFlux_3/totalVolume    
-    volumetricGradT_1 = volumetricGradT_1/totalVolume
-    volumetricGradT_2 = volumetricGradT_2/totalVolume
-    volumetricGradT_3 = volumetricGradT_3/totalVolume
-    volumetricTemp = volumetricTemp/totalVolume
+    volumetricTemp = volumetricTemp/totalVolume    
+    dT_dt_macroscopic = (volumetricTemp - volumetricTemp_initial)/time
+    specificHeat = volumetricHeatFlux/(dT_dt_macroscopic*density)
     
-    domainDimensionX = 1
-    dT_dt_macroscopic = (volumetricTemp - volumetricTemp_old)/(time - time_old)
-    gradientppliedResultantHeatFlux = appliedResultantHeatFlux/domainDimensionX
-    specificHeat = gradientAppliedResultantHeatFlux/dT_dt_macroscopic
-    specificHeat_ = ((volumetricHeatFlux_1-volumetricHeatFlux_1_old)/domainDimensionX)/dT_dt_macroscopic
+    print('data:',time,volumetricTemp,specificHeat)        
+    outputFileSpecificHeatEffective.write("{0:25.10f}{1:25.10f}{2:25.10f}{3:25.10f}{4:25.10f}\n".format(time,density,volumetricTemp_initial,volumetricTemp,specificHeat))
     
-    time_old = time
-    volumetricTemp_old = volumetricTemp
-    volumetricHeatFlux_1_old = volumetricHeatFlux_1
-    
-    print('data:',time,volumetricHeatFlux_1,volumetricHeatFlux_2,volumetricHeatFlux_3,volumetricTemp,specificHeat,specificHeat_)
-    
-       
-    #data = str(time) + '    ' + str(K1) + '    ' + str(K2) + '    ' + str(K3) + '\n'
-    #outputFileSpecificHeatEffective.write(data)
-    
-
-
 outputFileSpecificHeatEffective.close()
 odb.close()
 end = wallTime.clock()
-
 print("Time Taken: ",(end-start), "seconds\n")
