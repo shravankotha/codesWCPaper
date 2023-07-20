6fe027ed27291d6bcd00d6a72cec0729b887e8fb
shravan kotha <shravan.kotha@icmrl.net>
2023-07-20 10:45:41 -0400

    node sets on the face, edges and corners are now written to files

--- findNodeAndElementSetsOnBoundaryFromAbaqusInpFile.py
+++ findNodeAndElementSetsOnBoundaryFromAbaqusInpFile.py
@@ -5,7 +5,7 @@ import sys
 import os
 from parseAbqInpFileForNodalCoords import parseAbqFileForNodalCoordinates
 from parseAbqInpFileForElementConnectivity import parseAbqFileForElementConnectivity
-from findElementsOnDomainBoundary import findElementsOnDomainBoundary
+from findNodesAndElementsOnDomainBoundary import findNodesAndElementsOnDomainBoundary
 from findDomainBoundaries import findDomainBoundaries
 from writeElementSetsInAbaqusInpFormat import writeElementSetsInAbaqusInpFormat
 
@@ -17,18 +17,41 @@ def main():
     
     nameFile = str(sys.argv[1])
     nFacesElement = 4    
-    pathFileOut = 'elementSetsOnDomainBoundaries.inp'
+    pathFileOut = 'nodeAndElementSetsOnDomainBoundaries.inp'
     
     listNodeIDs, listNodalCoordinates = parseAbqFileForNodalCoordinates(nameFile)
     listElementIDs, listElementConnectivity = parseAbqFileForElementConnectivity(nameFile)
     listCoordMinDomainBoundary,listCoordMaxDomainBoundary = findDomainBoundaries(listNodeIDs, listNodalCoordinates)    
-    listNamesElementSets, listElementsOnBoundaries = findElementsOnDomainBoundary(listCoordMinDomainBoundary,listCoordMaxDomainBoundary,listElementIDs,listElementConnectivity,listNodalCoordinates,nFacesElement)    
+    listNamesElementSets, listElementsOnBoundaries, listNamesNodeSets, listNodesOnBoundaries = findNodesAndElementsOnDomainBoundary(listCoordMinDomainBoundary,listCoordMaxDomainBoundary,listElementIDs,listElementConnectivity,listNodalCoordinates,nFacesElement)    
+    success = writeNodeSetsInAbaqusInpFormat(pathFileOut,listNamesNodeSets,listNodesOnBoundaries)
     success = writeElementSetsInAbaqusInpFormat(pathFileOut,listNamesElementSets,listElementsOnBoundaries)
+
+def writeNodeSetsInAbaqusInpFormat(pathFileOut,listNamesNodeSets,listNodesOnBoundaries):
+    success = False
+    with open (pathFileOut,'w') as fileOut:
+        for iBoundary in range(0,len(listNamesElementSets)):
+            for iSurface in range(0,len(listNamesElementSets[0])):
+                if listElementsOnBoundaries[iBoundary][iSurface] != []:
+                    fileOut.write('*NSET, NSET=' + str(listNamesElementSets[iBoundary][iSurface]) + '\n')
+                    str_to_write = '    '
+                    for iElement in range(0,len(listElementsOnBoundaries[iBoundary][iSurface])):
+                        if iElement == len(listElementsOnBoundaries[iBoundary][iSurface])-1:
+                            str_to_write = str_to_write + str(listElementsOnBoundaries[iBoundary][iSurface][iElement])
+                        else:
+                            str_to_write = str_to_write + str(listElementsOnBoundaries[iBoundary][iSurface][iElement]) + ',   '                        
+                            
+                        if iElement != 0:
+                            if iElement%7 == 0 or iElement == len(listElementsOnBoundaries[iBoundary][iSurface])-1:
+                                fileOut.write(str_to_write + '\n')
+                                str_to_write = '    '
+    fileOut.close()
+    success = True
+    return success
     
     
 def writeElementSetsInAbaqusInpFormat(pathFileOut,listNamesElementSets,listElementsOnBoundaries):
     success = False
-    with open (pathFileOut,'w') as fileOut:
+    with open (pathFileOut,'a') as fileOut:
         for iBoundary in range(0,len(listNamesElementSets)):
             for iSurface in range(0,len(listNamesElementSets[0])):
                 if listElementsOnBoundaries[iBoundary][iSurface] != []:
@@ -47,12 +70,6 @@ def writeElementSetsInAbaqusInpFormat(pathFileOut,listNamesElementSets,listEleme
     fileOut.close()
     success = True
     return success
-    
-    
-   
-
-   
-
     
 if __name__ == "__main__":
     main()
